ec393f4a66594d23de612802b266d4a2561ce1db
Federico Magnani <magnani.federico@me.com>
2026-02-02 09:15:01 +0100

    add news section with dynamic rendering and styling

---
+++ src/Controller/NewsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/{_locale}/news')]
+final class NewsController extends AbstractController
+{
+
+    #[Route('', name: 'newsList')]
+    public function newsList(Request $request): Response
+    {
+        $newsList = []; // TODO: Fetch via Repository
+
+        return $this->render('public/news_list.html.twig', [
+            'newsList' => $newsList,
+        ]);
+    }
+
+}
