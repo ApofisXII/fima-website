caaf7acc5d42ddcf0128df55777507a29fcab31d
Federico Magnani <magnani.federico@me.com>
2026-02-19 01:35:16 +0100

    chores

--- src/Controller/Public/NewsController.php
+++ src/Controller/Public/NewsController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Public;
 
 use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
