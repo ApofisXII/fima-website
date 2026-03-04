357dfcbd534fc54bda223a4a2b57fa5496c47227
Federico Magnani <magnani.federico@me.com>
2026-03-04 23:45:48 +0100

    implement Quill editor for news body input and update styles

--- assets/admin.js
+++ assets/admin.js
@@ -1,1 +1,4 @@
 import './styles/admin.css';
+
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
--- importmap.php
+++ importmap.php
@@ -56,4 +56,32 @@ return [
     'tinymce-config' => [
         'path' => './assets/tinymce-config.js',
     ],
+    'quill' => [
+        'version' => '2.0.3',
+    ],
+    'lodash-es' => [
+        'version' => '4.17.21',
+    ],
+    'parchment' => [
+        'version' => '3.0.0',
+    ],
+    'quill-delta' => [
+        'version' => '5.1.0',
+    ],
+    'eventemitter3' => [
+        'version' => '5.0.1',
+    ],
+    'fast-diff' => [
+        'version' => '1.3.0',
+    ],
+    'lodash.clonedeep' => [
+        'version' => '4.5.0',
+    ],
+    'lodash.isequal' => [
+        'version' => '4.5.0',
+    ],
+    'quill/dist/quill.snow.css' => [
+        'version' => '2.0.3',
+        'type' => 'css',
+    ],
 ];
