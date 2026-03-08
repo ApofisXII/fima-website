f4156e6aa4ee48ee76fd8b07e0ebf98031d204f9
GiorgiousB <gio.bonafini@gmail.com>
2026-03-08 22:50:57 +0100

    price required

--- src/DTO/Admin/UrbinoCourseRequestDTO.php
+++ src/DTO/Admin/UrbinoCourseRequestDTO.php
@@ -34,7 +34,10 @@ class UrbinoCourseRequestDTO
 
         public ?string $dateEnd = null,
 
+        #[Assert\NotBlank(message: "Il prezzo è obbligatorio.")]
         public ?string $priceInfoIt = null,
+
+        #[Assert\NotBlank(message: "Il prezzo è obbligatorio.")]
         public ?string $priceInfoEn = null,
 
         #[Assert\NotBlank(message: "La materia del corso è obbligatoria.")]
