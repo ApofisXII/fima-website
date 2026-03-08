2cbe1d8ea3f4db9430efa452f9c67fd6274cef91
GiorgiousB <gio.bonafini@gmail.com>
2026-03-09 00:23:35 +0100

    removed edition enrollment link from admin

---
+++ migrations/Version20260308232133.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260308232133 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE urbino_edition DROP enrollment_link');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE urbino_edition ADD enrollment_link VARCHAR(2048) DEFAULT NULL');
+    }
+}
